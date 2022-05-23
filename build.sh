eb7a85049673deb13f266a953abd8b54bd5d8ebb
linepro6 <53991395+linepro6@users.noreply.github.com>
2022-05-23 14:37:39 +0800

    ci: try fix

--- build.sh
+++ build.sh
@@ -3,9 +3,8 @@ set -euo pipefail
 
 mkdir -p output
 
-alias py=/usr/bin/python3
+export PythonBin=/usr/bin/python3
 
 # The normal build
 dotnet build -c Release \
-    -p:PythonBin:/usr/bin/python3
     -p:OutputPath=../output/

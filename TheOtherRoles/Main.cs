4f5c6d915c820aa3f2d5f6c27fe87404be9302af
lingxiaoDY <106056135+lingxiaoDY@users.noreply.github.com>
2022-06-29 19:23:56 +0800

    修改版本号

--- TheOtherRoles/Main.cs
+++ TheOtherRoles/Main.cs
@@ -17,7 +17,7 @@ namespace TheOtherRoles
     public class TheOtherRolesPlugin : BasePlugin
     {
         public const string Id = "me.eisbison.theotherroles";
-        public const string VersionString = "2.1.63";
+        public const string VersionString = "2.1.64";
 
         public static System.Version Version = System.Version.Parse(VersionString);
         internal static BepInEx.Logging.ManualLogSource Logger;
